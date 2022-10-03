b67814dab305941336aad4b01bc9bd2fb1a90b8c
MelodRAMAtick <rmasseroni@uade.edu.ar>
2022-10-03 17:21:52 -0300

    Input and display v0.1

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
-import './App.css';
-import Header from './Header';
+import "./App.css";
+import Header from "./Header";
 
 export default function App() {
   return (
     <div className="App">
-      <Header/>
-      <h2>test</h2>
+      <Header />
+      <div className="body">
+        <div style={{backgroundColor: "gray", flex: 1, height: "39rem"}}>
+          <h2>test inputs</h2>
+        </div>
+        <div style={{backgroundColor: "turquoise", flex: 3}}>
+          <h2>test display</h2>
+        </div>
+      </div>
     </div>
   );
 }

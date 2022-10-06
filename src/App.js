267e265304b279cd6a7f3419bc112a00622c22e7
MelodRAMAtick <rmasseroni@uade.edu.ar>
2022-10-06 02:40:08 -0300

    Parameters now submittable

--- src/App.js
+++ src/App.js
@@ -3,20 +3,22 @@ import "./App.css";
 import Header from "./Header";
 import * as Yup from "yup";
 import { useEffect, useState } from "react";
-import { Slider, TextField } from "@mui/material";
+import { Button, Slider, TextField } from "@mui/material";
+import marks from './marks';
 
 const ParameterSchema = Yup.object().shape({
   equation: Yup.string(
     "Enter an equation. Make sure to use special symbols such as ^"
-  ).required(),
+  ).required("Este parámetro es obligatorio!"),
 
-  a: Yup.number("Enter the a value for the A matrix").integer().min(-5).max(5),
-  b: Yup.number("Enter the b value for the A matrix").integer().min(-5).max(5),
-  c: Yup.number("Enter the c value for the A matrix").integer().min(-5).max(5),
-  d: Yup.number("Enter the d value for the A matrix").integer().min(-5).max(5),
+  a: Yup.number("Enter the a value for the A matrix").required("Este parámetro es obligatorio!"),
+  b: Yup.number("Enter the b value for the A matrix"),
+  c: Yup.number("Enter the c value for the A matrix").required("Este parámetro es obligatorio!"),
+  d: Yup.number("Enter the d value for the A matrix"),
 
   // Autovectores 1 y 2; en principio serían arrays de 2 espacios
 });
+
 
 export default function App() {
   const [eigenvalue_1, setEV1] = useState([0, 0]);
@@ -26,6 +28,10 @@ export default function App() {
   //   console.log('Autovector 1: ' + eigenvalue_1);
   //   console.log('Autovector 2: ' + eigenvalue_2);
   // }, []);
+
+  function valueText(value) {
+    return `${value}°C`;
+  }
 
   const formik = useFormik({
     initialValues: {
@@ -50,7 +56,12 @@ export default function App() {
           <h2>test inputs</h2>
           <form onSubmit={formik.handleSubmit}>
             <TextField
-              fullWidth
+              color="secondary"
+              sx={{
+                backgroundColor: "white",
+                border: "1px solid #ced4da",
+                borderRadius: 1,
+              }}
               id="equation"
               name="equation"
               label="Equation"
@@ -59,50 +70,86 @@ export default function App() {
               error={formik.touched.equation && Boolean(formik.errors.equation)}
               helperText={formik.touched.equation && formik.errors.equation}
             />
+            <div className="slider">
+              <Slider
+                id="a"
+                name="a"
+                aria-label="a value"
+                getAriaValueText={valueText}
+                marks={marks}
+                step={0.25}
+                min={-5}
+                max={5}
+                defaultValue={formik.values.a}
+                value={formik.values.a}
+                onChange={formik.handleChange}
+                error={formik.touched.a && formik.errors.a}
+                helperText={formik.touched.a && formik.errors.a}
+                valueLabelDisplay="auto"
+              />
 
-            <Slider
-              fullWidth
-              id="a"
-              name="a"
-              label="a value"
-              value={formik.values.a}
-              onChange={formik.handleChange}
-              error={formik.touched.a && Boolean(formik.errors.a)}
-              helperText={formik.touched.a && formik.errors.a}
-            />
+              <Slider
+                className="slider"
+                id="b"
+                name="b"
+                aria-label="b value"
+                getAriaValueText={valueText}
+                marks={marks}
+                step={0.25}
+                min={-5}
+                max={5}
+                defaultValue={formik.values.b}
+                value={formik.values.b}
+                onChange={formik.handleChange}
+                error={formik.touched.b && formik.errors.b}
+                helperText={formik.touched.b && formik.errors.b}
+                valueLabelDisplay="auto"
+              />
 
-            <Slider
-              fullWidth
-              id="b"
-              name="b"
-              label="b value"
-              value={formik.values.b}
-              onChange={formik.handleChange}
-              error={formik.touched.b && Boolean(formik.errors.b)}
-              helperText={formik.touched.b && formik.errors.b}
-            />
+              <Slider
+                className="slider"
+                id="c"
+                name="c"
+                aria-label="c value"
+                getAriaValueText={valueText}
+                marks={marks}
+                step={0.25}
+                min={-5}
+                max={5}
+                defaultValue={formik.values.c}
+                value={formik.values.c}
+                onChange={formik.handleChange}
+                error={formik.touched.c && formik.errors.c}
+                helperText={formik.touched.c && formik.errors.c}
+                valueLabelDisplay="auto"
+              />
 
-            <Slider
-              fullWidth
-              id="c"
-              name="c"
-              label="c value"
-              value={formik.values.c}
-              onChange={formik.handleChange}
-              error={formik.touched.c && Boolean(formik.errors.c)}
-              helperText={formik.touched.c && formik.errors.c}
-            />
+              <Slider
+                className="slider"
+                id="d"
+                name="d"
+                aria-label="d value"
+                getAriaValueText={valueText}
+                marks={marks}
+                step={0.25}
+                min={-5}
+                max={5}
+                defaultValue={formik.values.d}
+                value={formik.values.d}
+                onChange={formik.handleChange}
+                error={formik.touched.d && formik.errors.d}
+                helperText={formik.touched.d && formik.errors.d}
+                valueLabelDisplay="auto"
+              />
+            </div>
 
-            <Slider
-              fullWidth
-              id="d"
-              name="d"
-              label="d value"
-              value={formik.values.d}
-              onChange={formik.handleChange}
-              error={formik.touched.d && Boolean(formik.errors.d)}
-              helperText={formik.touched.d && formik.errors.d}
-            />
+            <Button
+              variant="contained"
+              type="submit"
+              onSubmit={formik.handleSubmit}
+            >
+              Calcular
+            </Button>
           </form>
         </div>
         <div className="display">

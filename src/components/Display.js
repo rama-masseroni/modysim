de8600d7342d4bae2967c96fbc2d7b03ecfc8400
MelodRAMAtick <rmasseroni@uade.edu.ar>
2022-10-21 16:20:23 -0300

    Q vs P graph implemented, needs formatting

--- src/components/Display.js
+++ src/components/Display.js
@@ -5,11 +5,32 @@ import * as algebra from "algebra.js";
 import functionPlot from "function-plot";
 
 export default function Display(props) {
+  const eq1 = algebra.parse("q");
+  const eq2 = algebra.parse("(p^2)*(1/4)");
+  const q_p_graph = new algebra.Equation(eq1, eq2);
+  console.log(q_p_graph.toString());
+  const graph = algebra.toTex(q_p_graph);
 
-    const q = new algebra.Expression('q');
-    const p = new algebra.Expression('p');
-    const q_p = new algebra.Equation(q, new algebra.Fraction(p.pow(2,false),4));
-    const graph = algebra.toTex(q_p);    
+  functionPlot({
+    title: q_p_graph.toString(),
+    target: "#quadratic-with-options",
+    width: 580,
+    height: 400,
+    disableZoom: true,
+    xAxis: {
+      label: "p - axis",
+      domain: [-4, 4],
+    },
+    yAxis: {
+      label: "q - axis",
+      domain: [-2, 4],
+    },
+    data: [
+      {
+        fn: "y=(x^2)/4",
+      },
+    ],
+  });
 
   return (
     <>
@@ -17,7 +38,7 @@ export default function Display(props) {
         <a>Test equation child component</a>
       </div>
       <div className="graphs_container">
-        <a>Test graph container child component</a>
+        <div id = "quadratic-with-options"></div>
       </div>
     </>
   );

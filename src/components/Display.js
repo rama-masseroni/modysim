376b6da22b833956809d6e97b98d187fafa3b838
MelodRAMAtick <rmasseroni@uade.edu.ar>
2022-10-29 19:31:41 -0300

    Working? plotter

--- src/components/Display.js
+++ src/components/Display.js
@@ -1,4 +1,4 @@
-import React, { PureComponent } from "react";
+import React, { PureComponent, useEffect } from "react";
 import "../App.css";
 import katex from "katex";
 import * as algebra from "algebra.js";
@@ -11,26 +11,31 @@ export default function Display(props) {
   console.log(q_p_graph.toString());
   const graph = algebra.toTex(q_p_graph);
 
-  functionPlot({
-
-    target: "#quadratic-with-options",
-    width: 580,
-    height: 400,
-    disableZoom: true,
-    xAxis: {
-      label: "p - axis",
-      domain: [-4, 4],
-    },
-    yAxis: {
-      label: "q - axis",
-      domain: [-2, 4],
-    },
-    data: [
-      {
-        fn: "y=(x^2)/4",
+  useEffect(() => {
+    functionPlot({
+      title: q_p_graph.toString(),
+      target: "#quadratic-with-options",
+      width: 580,
+      height: 400,
+      disableZoom: true,
+      xAxis: {
+        label: "p - axis",
+        domain: [-4, 4],
       },
-    ],
-  });
+      yAxis: {
+        label: "q - axis",
+        domain: [-2, 4],
+      },
+      data: [
+        {
+          fn: "y=(x^2)/4",
+          graphType: "polyline",
+        },
+      ],
+    });
+
+    return () => {};
+  }, []);
 
   return (
     <>
@@ -38,7 +43,7 @@ export default function Display(props) {
         <a>Test equation child component</a>
       </div>
       <div className="graphs_container">
-        <div id = "quadratic-with-options"></div>
+        <div id="quadratic-with-options"></div>
       </div>
     </>
   );

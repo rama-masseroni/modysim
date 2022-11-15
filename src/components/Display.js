4ab4c1de8169c0580570d120eeb3aac2fe82674a
MelodRAMAtick <rmasseroni@uade.edu.ar>
2022-11-15 03:56:06 -0300

    Nulclines included; can't swap functionPlot data

--- src/components/Display.js
+++ src/components/Display.js
@@ -1,4 +1,4 @@
-import React, { PureComponent, useEffect } from "react";
+import React, { PureComponent, useEffect, useState } from "react";
 import "../App.css";
 import katex from "katex";
 import * as algebra from "algebra.js";
@@ -6,6 +6,8 @@ import functionPlot from "function-plot";
 import { CloudinaryContext, Image, Transformation } from "cloudinary-react";
 import { Matrix } from "ml-matrix";
 import { MathJax } from "better-react-mathjax";
+import { Fab } from "@mui/material";
+import { Update } from "@mui/icons-material";
 
 export default function Display({ p, q, url, ev1, ev2, values }) {
   const pp = p;
@@ -17,70 +19,141 @@ export default function Display({ p, q, url, ev1, ev2, values }) {
   console.log(q_p_graph.toString());
   const graph = algebra.toTex(q_p_graph);
 
+  const [graphView, setGraphView] = useState(false);
+  // var options = {
+  //   target: "#quadratic-with-options",
+  //   width: 500,
+  //   height: 455,
+  //   grid: true,
+  //   disableZoom: true,
+
+  //   xAxis: {
+  //     label: "p - axis",
+  //     domain: [-4, 4],
+  //   },
+  //   yAxis: {
+  //     label: "q - axis",
+  //     domain: [-4, 4],
+  //   },
+  // };
+  console.log(values);
+
+  const xx = new algebra.Expression("x");
+  const xy = new algebra.Expression("y");
+
+  const xaux = new algebra.Equation(
+    xx.multiply(values[0]).add(xy.multiply(values[1])),
+    0
+  );
+  const xdot = xaux.solveFor("y");
+  console.log(xdot.toString());
+
+  const yaux = new algebra.Equation(
+    xx.multiply(values[2]).add(xy.multiply(values[3])),
+    0
+  );
+  const ydot = yaux.solveFor("y");
+  console.log(ydot.toString());
+
   console.log(ev1);
   console.log(ev2);
 
   useEffect(() => {
-    console.log(qq);
-    console.log(pp);
-    console.log(imagen);
-    functionPlot({
-      // title: q_p_graph.toString(),
-      target: "#quadratic-with-options",
-      width: 500,
-      height: 455,
-      grid: true,
-      disableZoom: true,
+    console.log(graphView);
 
-      xAxis: {
-        label: "p - axis",
-        domain: [-4, 4],
-      },
-      yAxis: {
-        label: "q - axis",
-        domain: [-4, 4],
-      },
-      data: [
-        {
-          fn: "y=(x^2)/4",
-          graphType: "polyline",
-          skipTip: true,
+    if (graphView === false) {
+      // delete options.data;
+      functionPlot({
+        target: "#quadratic-with-options",
+        width: 500,
+        height: 455,
+        grid: true,
+        disableZoom: true,
+    
+        xAxis: {
+          label: "p - axis",
+          domain: [-4, 4],
         },
-        {
-          points: [[pp, qq]],
-          fnType: "points",
-          graphType: "scatter",
-          color: "black",
+        yAxis: {
+          label: "q - axis",
+          domain: [-4, 4],
         },
-        {
-          vector: [ev1[0], ev1[1]],
-          graphType: "polyline",
-          fnType: "vector",
-          color: "red",
+        data: [
+          {
+            fn: "y=(x^2)/4",
+            graphType: "polyline",
+            skipTip: true,
+          },
+        ]
+      });
+    } else {
+      // delete options.data;
+      functionPlot({
+        target: "#quadratic-with-options",
+        width: 500,
+        height: 455,
+        grid: true,
+        disableZoom: true,
+    
+        xAxis: {
+          label: "p - axis",
+          domain: [-4, 4],
         },
-        {
-          vector: [-ev1[0], -ev1[1]],
-          graphType: "polyline",
-          fnType: "vector",
-          color: "red",
+        yAxis: {
+          label: "q - axis",
+          domain: [-4, 4],
         },
-        {
-          vector: [ev2[0], ev2[1]],
-          graphType: "polyline",
-          fnType: "vector",
-          color: "green",
-        },
-        {
-          vector: [-ev2[0], -ev2[1]],
-          graphType: "polyline",
-          fnType: "vector",
-          color: "green",
-        },
-      ],
-    });
+        data: [
+          {
+            fn: "y=" + xdot.toString(),
+            graphType: "scatter",
+            nSamples: 100,
+            skipTip: true,
+            color: "purple",
+          },
+          {
+            fn: "y=" + ydot.toString(),
+            graphType: "scatter",
+            nSamples: 100,
+            skipTip: true,
+            color: "magenta",
+          },
+          {
+            points: [[pp, qq]],
+            fnType: "points",
+            graphType: "polyline",
+            color: "black",
+          },
+          {
+            vector: [ev1[0], ev1[1]],
+            graphType: "polyline",
+            fnType: "vector",
+            color: "red",
+          },
+          {
+            vector: [-ev1[0], -ev1[1]],
+            graphType: "polyline",
+            fnType: "vector",
+            color: "red",
+          },
+          {
+            vector: [ev2[0], ev2[1]],
+            graphType: "polyline",
+            fnType: "vector",
+            color: "green",
+          },
+          {
+            vector: [-ev2[0], -ev2[1]],
+            graphType: "polyline",
+            fnType: "vector",
+            color: "green",
+          },
+        ]
+      });
+    }
+    // console.log(options);
 
-    return () => {};
-  }, []);
+  }, [graphView]);
 
   return (
     <>
@@ -98,14 +171,21 @@ export default function Display({ p, q, url, ev1, ev2, values }) {
           {values[2]}
           {"*x + "}
           {values[3]}
-          {'*y \\end{cases}\\)'}
+          {"*y \\end{cases}\\)"}
         </MathJax>
       </div>
       <div className="graphs_container">
-        <div
-          id="quadratic-with-options"
-          style={{ backgroundColor: "white" }}
-        ></div>
+        <div style={{ display: "flex", alignItems: "flex-end" }}>
+          <div
+            id="quadratic-with-options"
+            style={{
+              backgroundColor: "white",
+            }}
+          ></div>
+          <Fab id="test" onClick={() => setGraphView(!graphView)}>
+            <Update />
+          </Fab>
+        </div>
         <div className="generic_graphs">
           <CloudinaryContext cloudName="dv57wpzku">
             <div>
